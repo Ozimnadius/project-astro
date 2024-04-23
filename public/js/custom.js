f79d1b054f3e9e3750eed4fd8e3208e6e02e580c
Mikle Seregin <mikhail-seregin@yandex.ru>
2024-04-23 16:20:20 +0300

    update

---
+++ public/js/custom.js
@@ -0,0 +1,3 @@
+window.addEventListener('DOMContentLoaded', function (){
+    console.log('Custom.js');
+});
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,23 @@
+import 'normalize.css';
+import '/node_modules/ozimnad-reset/reset.css';
+
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
+
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+
+
+import '../styles/styles.scss';
+
+
 window.addEventListener('DOMContentLoaded', function (){
    console.log('Это общий JS');
+});
+
+Fancybox.bind("[data-fancybox]", {
+   // Your custom options
 });
